cdaab6721f7d518b702f37b95f7d012cb741622c
JeffM <jeff@jeffreymclean.com>
2019-09-24 08:42:58 -0700

    Added the Spotify function to find song info

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios")
-var reqSpotify = require("node-spotify-api"); 
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
 var moment = require("moment");
@@ -12,22 +12,28 @@ var moment = require("moment");
 var inputString = process.argv;
 var userCommand = inputString[2];
 
-console.log(inputString)
+// console.log(inputString)
 
 
 // ------------- Checks to see Which operation was Called -------------
-if (userCommand === "concert-this"){
+if (userCommand === "concert-this") {
     var artist = process.argv.slice(3).join("+");
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(resp){
-    console.log(resp.data);
-    console.log("Next Concert: " + moment(resp.data[0].datetime).format("dddd, MMMM Do YYYY, h:mm:a"));
-    console.log("Venue: " + resp.data[0].venue.name)
-    console.log("Location: " + resp.data[0].venue.city + ", " + resp.data[0].venue.country)
-    }).catch(function(err){
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (resp) {
+        //console.log(resp.data);
+        console.log("Next Concert: " + moment(resp.data[0].datetime).format("dddd, MMMM Do YYYY, h:mm:a"));
+        console.log("Venue: " + resp.data[0].venue.name)
+        console.log("Location: " + resp.data[0].venue.city + ", " + resp.data[0].venue.country)
+    }).catch(function (err) {
         console.log(err);
     })
 }
-else if(userCommand === "spotify-this-song"){
+else if (userCommand === "spotify-this-song") {
     var song = process.argv.slice(3).join("+");
-
+    spotify.search({ type: 'track', query: song }).then(function (resp) {
+        var album = resp.tracks.items[0].album.name;
+        var songArtist = resp.tracks.items[0].album.artists[0].name
+        console.log ( )
+        console.log("Performing Artist: " + songArtist)
+        console.log("From their Album: " + album);
+    })
 }
