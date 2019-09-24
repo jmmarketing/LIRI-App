543f6dc6f9b20eddeee9b1c2eb4f6fb7e1853d85
JeffM <jeff@jeffreymclean.com>
2019-09-23 17:46:15 -0700

    Created if statement and function for when 'concert-this' is called

--- liri.js
+++ liri.js
@@ -1,9 +1,33 @@
-require("dotenv").config();
-
-
 // ------------- Required Modules ------------
+require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios")
+var reqSpotify = require("node-spotify-api"); 
 var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
 var moment = require("moment");
+
+// ------------ Global Variables ------------
+
+var inputString = process.argv;
+var userCommand = inputString[2];
+
+console.log(inputString)
+
+
+// ------------- Checks to see Which operation was Called -------------
+if (userCommand === "concert-this"){
+    var artist = process.argv.slice(3).join("+");
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(resp){
+    console.log(resp.data);
+    console.log("Next Concert: " + moment(resp.data[0].datetime).format("dddd, MMMM Do YYYY, h:mm:a"));
+    console.log("Venue: " + resp.data[0].venue.name)
+    console.log("Location: " + resp.data[0].venue.city + ", " + resp.data[0].venue.country)
+    }).catch(function(err){
+        console.log(err);
+    })
+}
+else if(userCommand === "spotify-this-song"){
+    var song = process.argv.slice(3).join("+");
+
+}
