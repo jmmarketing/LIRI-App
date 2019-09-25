c0ebb7725ecb80781fa6bbb5efd7285d10ad2bbf
JeffM <jeff@jeffreymclean.com>
2019-09-24 21:40:17 -0700

    Added the Do What It Says action. Cleaned up the code by adding functions and usine switch/case

--- liri.js
+++ liri.js
@@ -11,15 +11,14 @@ var moment = require("moment");
 
 var inputString = process.argv;
 var userCommand = inputString[2];
+var searchItem = process.argv.slice(3).join("+");
 
 // console.log(inputString)
 
 
-// ------------- Checks to see Which operation was Called -------------
-// ------------ Checks for Cocnert This Action--------------------
-if (userCommand === "concert-this") {
-    var artist = process.argv.slice(3).join("+");
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (resp) {
+// -------------- Concert This Function -----------------------
+function concertThis() {
+    axios.get("https://rest.bandsintown.com/artists/" + searchItem + "/events?app_id=codingbootcamp").then(function (resp) {
         //console.log(resp.data);
         console.log("Next Concert: " + moment(resp.data[0].datetime).format("dddd, MMMM Do YYYY, h:mm:a"));
         console.log("Venue: " + resp.data[0].venue.name)
@@ -29,11 +28,13 @@ if (userCommand === "concert-this") {
             console.log(err);
         })
 }
-// --------------- Check for Spotify Action --------------
-else if (userCommand === "spotify-this-song") {
-    var song = process.argv.slice(3).join("+");
-    
-    spotify.search({ type: 'track', query: song }).then(function (resp) {
+// ------------------------------------------------------
+
+
+
+// -------------- Spotify This Function -----------------------
+function spotifyThis() {
+    spotify.search({ type: 'track', query: searchItem }).then(function (resp) {
         var album = resp.tracks.items[0].album.name;
         var songArtist = resp.tracks.items[0].album.artists[0].name;
         var songTitle = resp.tracks.items[0].name;
@@ -47,26 +48,78 @@ else if (userCommand === "spotify-this-song") {
         console.log("You must specify a song")
         console.log(err);
     })
+}
+// ------------------------------------------------------
 
-// ------------------ Checks for Movie Action ---------------
-}else if (userCommand === "movie-this") {
-    var movie = process.argv.slice(3).join("+");
-    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie + "&type=movie").then(function (resp) {
-        console.log ("Movie Title: "+ resp.data.Title)
-        console.log ("Year: "+ resp.data.Year)
-        console.log ("IMDB Rating: "+ resp.data.Ratings[0].Value)
-        console.log ("Rotten Tomatoes Rating: "+ resp.data.Ratings[1].Value)
-        console.log ("Country: "+ resp.data.Country)
-        console.log ("Language(s): "+ resp.data.Language)
-        console.log ("Plot: "+ resp.data.Plot)
-        console.log ("Actors: "+ resp.data.Actors)
+
+
+// -------------- Movie This Function -----------------------
+function movieThis() {
+
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + searchItem + "&type=movie").then(function (resp) {
+        //console.log(resp.data);
+        console.log("Movie Title: " + resp.data.Title)
+        console.log("Year: " + resp.data.Year)
+        console.log("IMDB Rating: " + resp.data.Ratings[0].Value)
+        console.log("Rotten Tomatoes Rating: " + resp.data.Ratings[1].Value)
+        console.log("Country: " + resp.data.Country)
+        console.log("Language(s): " + resp.data.Language)
+        console.log("Plot: " + resp.data.Plot)
+        console.log("Actors: " + resp.data.Actors)
+
     })
         .catch(function (err) {
             console.log(err);
         })
-}
-// ---------------------- Checsk for do What It Says Action ---------------
-else if (userCommand === "do-what-it-says"){
-    
-}
 
+}
+// ------------------------------------------------------
+
+
+// -------------- Do What It Says Function -----------------------
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+        userCommand = dataArr[0];
+        searchItem = dataArr[1];
+        if (userCommand === "concert-this") {
+            concertThis();
+        }
+        // --------------- Check for Spotify Action --------------
+        else if (userCommand === "spotify-this-song") {
+            spotifyThis();
+            // ------------------ Checks for Movie Action ---------------
+        } else if (userCommand === "movie-this") {
+            movieThis();
+        }
+
+    })
+}
+//---------------------------------------------------------------------
+
+
+// -------------------------- START EXECUTIION -----------------------
+// ------------- Checks to see Which operation was Called -------------
+// --------------------------------------------------------------------
+
+switch (userCommand) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+}
